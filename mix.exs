770c6bc12a29c2be05b903f4efbcba5447fb2608
Cameron Bytheway <bytheway.cameron@gmail.com>
2016-12-20 00:24:40 -0700

    release 0.1.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Concerto.Plug.Mixfile do
   def project do
     [app: :concerto_plug,
      description: "Plug integration for Concerto",
-     version: "0.1.2",
+     version: "0.1.3",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
